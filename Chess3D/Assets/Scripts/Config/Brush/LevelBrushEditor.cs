61be45e7c3a3a47818d0ec2ed5e81e9ce4bf8184
unknown <quangnhan1411@gmail.com>
2024-06-25 15:47:03 +0700

    Add LevelBrush

---
+++ Chess3D/Assets/Scripts/Config/Brush/LevelBrushEditor.cs
@@ -0,0 +1,50 @@
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LevelBrush))]
+public class LevelBrushEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        LevelBrush brush = (LevelBrush)target;
+
+        EditorGUILayout.LabelField("Prefab List", EditorStyles.boldLabel);
+
+        // Display the list of prefabs
+        for (int i = 0; i < brush.prefabs.Count; i++)
+        {
+            EditorGUILayout.BeginHorizontal();
+            brush.prefabs[i] = (GameObject)EditorGUILayout.ObjectField(brush.prefabs[i], typeof(GameObject), false);
+
+            if (GUILayout.Button("Remove"))
+            {
+                brush.prefabs.RemoveAt(i);
+            }
+            EditorGUILayout.EndHorizontal();
+        }
+
+        if (GUILayout.Button("Add Prefab"))
+        {
+            brush.prefabs.Add(null);
+        }
+
+        // Display a dropdown to select the current prefab
+        EditorGUILayout.Space();
+        brush.selectedIndex = EditorGUILayout.Popup("Selected Prefab", brush.selectedIndex, brush.prefabs.ConvertAll(p => p != null ? p.name : "None").ToArray());
+
+        // Checkbox for manual offset
+        brush.manualOffset = EditorGUILayout.Toggle("Manual Offset", brush.manualOffset);
+
+        if (brush.manualOffset)
+        {
+            // Field for manual offset value
+            brush.manualOffsetValue = EditorGUILayout.IntField("Manual Offset Value", brush.manualOffsetValue);
+        }
+
+        // Save changes
+        if (GUI.changed)
+        {
+            EditorUtility.SetDirty(brush);
+        }
+    }
+}
