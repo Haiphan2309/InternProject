c8e9cb1ebe8b776f5e9fa716376d0e4ebf7cadee
Fragalis <127601973+Fragalis@users.noreply.github.com>
2024-06-25 14:56:12 +0700

    add Vector3 param in Move()

--- Chess3D/Assets/Scripts/Config/Chess Pieces/PawnConfig.cs
+++ Chess3D/Assets/Scripts/Config/Chess Pieces/PawnConfig.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 
 public class PawnConfig : ChessManConfig
 {
+    private float[] XmovementList = { 1f, 0f, -1f, 0f };
+    private float[] ZmovementList = { 0f, 1f, 0f, -1f };
     PawnConfig()
     {
         Debug.Log("Spawn Pawn");
         this.chessManType = GDC.Enums.ChessManType.PAWN;
     }
-    public override List<Vector3> Move()
+    public override List<Vector3> Move(Vector3 currentPosIndex)
     {
-
+        List<Vector3> moveableList = new List<Vector3>();
+        for(int i = 0; i < XmovementList.Length; ++i)
+        {
+            moveableList.Add(currentPosIndex + Vector3.forward * ZmovementList[i] + Vector3.right * XmovementList[i]);
+        }
+        return moveableList;
     }
 }
