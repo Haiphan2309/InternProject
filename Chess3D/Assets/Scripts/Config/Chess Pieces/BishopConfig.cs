a926f54c2b1fbf573e3944bfffc7b7589b495e9f
Fragalis <127601973+Fragalis@users.noreply.github.com>
2024-06-25 16:20:06 +0700

    every ChessManConfig added without ValidateMove()

--- Chess3D/Assets/Scripts/Config/Chess Pieces/BishopConfig.cs
+++ Chess3D/Assets/Scripts/Config/Chess Pieces/BishopConfig.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "PawnConfig", menuName = "ChessManConfig/PawnConfig", order = 0)]
-public class PawnConfig : ChessManConfig
+[CreateAssetMenu(fileName = "BishopConfig", menuName = "ChessManConfig/BishopConfig", order = 3)]
+public class BishopConfig : ChessManConfig
 {
     public GameObject prefab;
-    private int _moveRange = 1;    // hard-coded number
-    private float[,] _straghtDirection = { { 0f, 1f }, { -1f, 0f }, { 1f, 0f }, { 0f, -1f } };
-    PawnConfig()
+    private int _moveRange = 16;    // hard-coded number
+    private float[,] _diagonalDirection = { { -1f, 1f },  { 1f, 1f }, { -1f, -1f }, { 1f, -1f } };
+
+    BishopConfig()
     {
-        Debug.Log("Spawn Pawn");
-        this.chessManType = GDC.Enums.ChessManType.PAWN;
+        Debug.Log("Spawn Bishop");
+        this.chessManType = GDC.Enums.ChessManType.BISHOP;
         this.possibleMoveList = new List<Vector3>();
     }
 
@@ -36,9 +37,9 @@ public class PawnConfig : ChessManConfig
     }
     public override void GenerateMoveList(Vector3 currentPositionIndex)
     {
-        for (int i = 0; i < _straghtDirection.Length; ++i)
+        for (int i = 0; i < _diagonalDirection.Length; ++i)
         {
-            Vector3 direction = Vector3.right * _straghtDirection[i, 0] + Vector3.forward * _straghtDirection[i, 1];
+            Vector3 direction = Vector3.right * _diagonalDirection[i, 0] + Vector3.forward * _diagonalDirection[i, 1];
             GenerateMove(currentPositionIndex, direction);
         }
     }
