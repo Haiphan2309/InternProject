c97d35f44d828c2d35405ef81ee7cd658fd272b8
Haiphan2309 <haiphan.230903@gmail.com>
2024-06-26 15:04:09 +0700

    backup

--- Chess3D/Assets/Scripts/Gameplay/GameplayManager.cs
+++ Chess3D/Assets/Scripts/Gameplay/GameplayManager.cs
@@ -11,6 +11,8 @@ public class GameplayManager : MonoBehaviour
 
     [SerializeField, ReadOnly] bool enemyTurn;
     public LevelData levelData;
+    [SerializeField] Transform availableMovePrefab;
+    List<Transform> availableMoveTrans = new List<Transform>();
     private void Awake()
     {
         Instance = this;
@@ -32,10 +34,12 @@ public class GameplayManager : MonoBehaviour
     void EnemyTurn()
     {
         //dosomething
+        Debug.Log("Enemy Turn!");
         ChangeTurn(false);
     }
     void PlayerTurn()
     {
+        Debug.Log("Player Turn");
         //dosomething
     }
     public void ShowAvailableMove(ChessManConfig chessManConfig, Vector3 curPosIndex)
@@ -46,7 +50,43 @@ public class GameplayManager : MonoBehaviour
             return;
         }
         List<Vector3> moves = chessManConfig.Move(curPosIndex);
-        //todo
+        if (moves != null)
+        {
+            foreach (Vector3 move in moves)
+            {
+                TileInfo tileInfo = levelData.GetTileInfo()[(int)move.x, (int)move.y, (int)move.z];
+                Transform tran = Instantiate(availableMovePrefab, move, Quaternion.identity);
+                switch (tileInfo.tileType)
+                {
+                    case TileType.GROUND:
+                        tran.position += new Vector3(0.5f, 0, 0.5f);
+                        break; ;
+                    case TileType.SLOPE_0:
+                        tran.position += new Vector3(0.5f, 0, 0.5f);
+                        break;
+                    case TileType.SLOPE_90:
+                        tran.position += new Vector3(0.5f, 0, 0.5f);
+                        break;
+                    case TileType.SLOPE_180:
+                        tran.position += new Vector3(0.5f, 0, 0.5f);
+                        break;
+                    case TileType.SLOPE_270:
+                        tran.position += new Vector3(0.5f, 0, 0.5f);
+                        break;
+                }
+                availableMoveTrans.Add(tran);
+            }
+
+            if (moves.Count == 0)
+            {
+                Debug.Log("Ko co nuoc di nao co the thuc hien");
+            }
+        }
+
+        if (moves == null)
+        {
+            Debug.LogError("move is null");
+        }
     }
     public bool CheckMove(ChessManConfig chessManConfig, Vector3 curPosIndex, Vector3 posIndexToMove)
     {
