c0cd6c847067d79fbcbba8d2c7998a0158084c4e
QuantumMarshal <quangnhan1411@gmail.com>
2024-06-28 16:37:56 +0700

    Update Chessman Anim

--- Chess3D/Assets/Scripts/Gameplay/ChessMan.cs
+++ Chess3D/Assets/Scripts/Gameplay/ChessMan.cs
@@ -5,7 +5,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 using UnityEngine.UIElements;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class ChessMan : MonoBehaviour
 {
@@ -85,10 +87,10 @@ public class ChessMan : MonoBehaviour
     }
     void KnightMoveAnim(Vector3 posIndexToMove)
     {
-        //transform.DOJump(posIndexToMove, 3, 1, 1).SetEase(Ease.InOutSine).OnComplete(()=>
-        //{
-        //    AjustPosToGround(transform.position, posIndexToMove, true);
-        //});   
+        transform.DOJump(posIndexToMove, 3, 1, 1).SetEase(Ease.InOutSine).OnComplete(() =>
+        {
+            AjustPosToGround(transform.position, posIndexToMove, true);
+        });
     }
 
     void OtherMoveAnim(Vector3 posIndexToMove)
@@ -99,7 +101,19 @@ public class ChessMan : MonoBehaviour
     private TileType GetTile(Vector3 position)
     {
         float Xpos = position.x;
-        float Ypos = position.y-1f;
+        float Ypos = position.y;
+        float Zpos = position.z;
+        return GameplayManager.Instance.levelData.GetTileInfo()[
+               (int)Mathf.Round(Xpos),
+               (int)Mathf.Round(Ypos),
+               (int)Mathf.Round(Zpos)
+               ].tileType;
+    }
+
+    private TileType GetChess(Vector3 position)
+    {
+        float Xpos = position.x;
+        float Ypos = position.y - 1f;
         float Zpos = position.z;
         return GameplayManager.Instance.levelData.GetTileInfo()[
                (int)Mathf.Round(Xpos),
@@ -111,44 +125,54 @@ public class ChessMan : MonoBehaviour
     IEnumerator Cor_OtherMoveAnim(Vector3 target)
     {
         Vector3 initPos = SnapToGrid(transform.position);
-        Vector3 direction = (target - initPos).normalized;
+        Vector3 direction = (target - initPos);
 
-        while (direction != Vector3.zero)
-        { 
-            AjustPosToGround(transform.position, target, direction);
-            direction = (target - SnapToGrid(transform.position)).normalized;
-            yield return null;
+        List<Vector3> path = CalculatePath(transform.position, target);
+
+        foreach (var gridCell in path)
+        {
+            while (transform.position != gridCell)
+            {
+                AjustPosToGround(transform.position, gridCell);
+                
+                yield return null;
+            }
         }
 
-        AjustPosToGround(transform.position, target, direction, true);
+        AjustPosToGround(transform.position, target, true);
         yield return new WaitForSeconds(1);
         GameplayManager.Instance.ChangeTurn();
     }
 
-    void AjustPosToGround(Vector3 newPosition, Vector3 target, Vector3 direction, bool isRoundInteger = false)
-    {
-        //RaycastHit hit;
-        //if (Physics.Raycast(transform.position + Vector3.up / 2f, Vector3.down, out hit, 0.6f, groundLayerMask))
-        //{
-        //    newPosition = Vector3.MoveTowards(transform.position, target, speed );
-        //    // newPosition = target;
-        //    newPosition.y = hit.point.y;
+    void AjustPosToGround(Vector3 newPosition, Vector3 target, bool isRoundInteger = false)
+    {   
+        newPosition = Vector3.MoveTowards(transform.position, target, 5f * Time.deltaTime);
+        Vector3 rotation = transform.rotation.eulerAngles;
 
-        //    Vector3 slopeRotation = Quaternion.FromToRotation(transform.up, hit.normal).eulerAngles;
+        TileType tileType = GetChess(newPosition);
+        switch (tileType) {
+            case TileType.SLOPE_0:
+                rotation.z = -45;
+                break;
 
-        //    // transform.DORotate(slopeRotation, 0.3f);
-        //}
-        //else
-        //{
-        //    Debug.Log("No ground detected");
-        //    newPosition += Vector3.down * 10 * Time.deltaTime;
-        //}
+            case TileType.SLOPE_90:
+                rotation.x = 45;
+                break;
 
-        TileType tileType = GetTile(newPosition);
-        Debug.Log(tileType);
-        Debug.Log(direction);
-        newPosition += direction;
+            case TileType.SLOPE_180:
+                Time.timeScale = 0.1f;
+                rotation.x = -45;
+                break;
 
+            case TileType.SLOPE_270:
+                rotation.z = 45;
+                break;
+
+            default:
+                rotation = Vector3.zero;
+                break;
+        }
+        
 
 
         if (isRoundInteger)
@@ -159,11 +183,84 @@ public class ChessMan : MonoBehaviour
         {
             transform.position = newPosition;
         }
+
+        transform.DORotate(rotation, 0.1f);
     }
 
     Vector3 SnapToGrid(Vector3 position)
     {
         return new Vector3(Mathf.Floor(position.x), Mathf.Floor(position.y), Mathf.Floor(position.z));
+    }
+
+    List<Vector3> CalculatePath(Vector3 start, Vector3 end)
+    {
+        List<Vector3> path = new List<Vector3>();
+        Vector3 current = start;
+
+        while (current != end) 
+        {
+            Vector3 tileUp = current + Vector3.up;
+            TileType tileType = GetChess(tileUp);
+
+            if (CheckSlope(tileType))
+            {
+                Debug.Log("Im in SLope");
+                current.y += 1;
+                path.Add(new Vector3(current.x, current.y, current.z));
+
+                if (current.x != end.x)
+                {
+                    current.x += Mathf.Sign(end.x - current.x);
+                }
+                if (current.z != end.z)
+                {
+                    current.z += Mathf.Sign(end.z - current.z);
+                }
+
+                continue;
+            }
+
+            tileType = GetChess(current);
+            if (tileType == TileType.NONE)
+            {
+                current.y -= 1;
+                path.Add(new Vector3(current.x, current.y, current.z));
+                continue;
+            }
+
+            else if (CheckSlope(tileType))
+            {
+                current.y -= 1;
+                if (current.x != end.x)
+                {
+                    current.x += Mathf.Sign(end.x - current.x);
+                }
+                if (current.z != end.z)
+                {
+                    current.z += Mathf.Sign(end.z - current.z);
+                }
+                path.Add(new Vector3(current.x, current.y, current.z));
+                continue;
+            }
+
+            if (current.x != end.x)
+            {
+                current.x += Mathf.Sign(end.x - current.x);
+            }
+            if (current.z != end.z)
+            {
+                current.z += Mathf.Sign(end.z - current.z);
+            }
+
+            path.Add(new Vector3(current.x, current.y, current.z));
+        }
+
+        return path;
+    }
+
+    bool CheckSlope(TileType tileType)
+    {
+        return tileType == TileType.SLOPE_0 || tileType == TileType.SLOPE_90 || tileType == TileType.SLOPE_180 || tileType == TileType.SLOPE_270;
     }
 
     [Button]
