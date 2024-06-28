53a3e5b0c6e17b027e1f4386f2dcc2769a77432c
QuantumMarshal <quangnhan1411@gmail.com>
2024-06-28 13:40:20 +0700

    Update Chessman

--- Chess3D/Assets/Scripts/Gameplay/ChessMan.cs
+++ Chess3D/Assets/Scripts/Gameplay/ChessMan.cs
@@ -5,6 +5,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Drawing;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ChessMan : MonoBehaviour
 {
@@ -84,10 +85,10 @@ public class ChessMan : MonoBehaviour
     }
     void KnightMoveAnim(Vector3 posIndexToMove)
     {
-        transform.DOJump(posIndexToMove, 3, 1, 1).SetEase(Ease.InOutSine).OnComplete(()=>
-        {
-            AjustPosToGround(transform.position, posIndexToMove, true);
-        });   
+        //transform.DOJump(posIndexToMove, 3, 1, 1).SetEase(Ease.InOutSine).OnComplete(()=>
+        //{
+        //    AjustPosToGround(transform.position, posIndexToMove, true);
+        //});   
     }
 
     void OtherMoveAnim(Vector3 posIndexToMove)
@@ -95,41 +96,60 @@ public class ChessMan : MonoBehaviour
         StartCoroutine(Cor_OtherMoveAnim(posIndexToMove));
     }
 
+    private TileType GetTile(Vector3 position)
+    {
+        float Xpos = position.x;
+        float Ypos = position.y-1f;
+        float Zpos = position.z;
+        return GameplayManager.Instance.levelData.GetTileInfo()[
+               (int)Mathf.Round(Xpos),
+               (int)Mathf.Round(Ypos),
+               (int)Mathf.Round(Zpos)
+               ].tileType;
+    }
+
     IEnumerator Cor_OtherMoveAnim(Vector3 target)
     {
-        Vector3 currentPos = SnapToGrid(transform.position);
-        Vector3 direction = (target - currentPos).normalized;
+        Vector3 initPos = SnapToGrid(transform.position);
+        Vector3 direction = (target - initPos).normalized;
 
-        float distance = Vector3.Distance(transform.position, target);
-        while (distance >= 0.05f)
-        {
-            AjustPosToGround(transform.position, target);
-            distance = Vector3.Distance(transform.position, target);
+        while (direction != Vector3.zero)
+        { 
+            AjustPosToGround(transform.position, target, direction);
+            direction = (target - SnapToGrid(transform.position)).normalized;
             yield return null;
         }
-        AjustPosToGround(transform.position, target, true);
+
+        AjustPosToGround(transform.position, target, direction, true);
         yield return new WaitForSeconds(1);
         GameplayManager.Instance.ChangeTurn();
     }
 
-    void AjustPosToGround(Vector3 newPosition, Vector3 target, bool isRoundInteger = false)
+    void AjustPosToGround(Vector3 newPosition, Vector3 target, Vector3 direction, bool isRoundInteger = false)
     {
-        RaycastHit hit;
-        if (Physics.Raycast(transform.position + Vector3.up / 2f, Vector3.down, out hit, 0.6f, groundLayerMask))
-        {
-            newPosition = Vector3.MoveTowards(transform.position, target, speed );
-            // newPosition = target;
-            newPosition.y = hit.point.y;
+        //RaycastHit hit;
+        //if (Physics.Raycast(transform.position + Vector3.up / 2f, Vector3.down, out hit, 0.6f, groundLayerMask))
+        //{
+        //    newPosition = Vector3.MoveTowards(transform.position, target, speed );
+        //    // newPosition = target;
+        //    newPosition.y = hit.point.y;
 
-            Vector3 slopeRotation = Quaternion.FromToRotation(transform.up, hit.normal).eulerAngles;
+        //    Vector3 slopeRotation = Quaternion.FromToRotation(transform.up, hit.normal).eulerAngles;
 
-            // transform.DORotate(slopeRotation, 0.3f);
-        }
-        else
-        {
-            Debug.Log("No ground detected");
-            newPosition += Vector3.down * 10 * Time.deltaTime;
-        }
+        //    // transform.DORotate(slopeRotation, 0.3f);
+        //}
+        //else
+        //{
+        //    Debug.Log("No ground detected");
+        //    newPosition += Vector3.down * 10 * Time.deltaTime;
+        //}
+
+        TileType tileType = GetTile(newPosition);
+        Debug.Log(tileType);
+        Debug.Log(direction);
+        newPosition += direction;
+
+
 
         if (isRoundInteger)
         {
@@ -144,33 +164,6 @@ public class ChessMan : MonoBehaviour
     Vector3 SnapToGrid(Vector3 position)
     {
         return new Vector3(Mathf.Floor(position.x), Mathf.Floor(position.y), Mathf.Floor(position.z));
-    }
-
-    List<Vector3> CalculatePath(Vector3 start, Vector3 end)
-    {
-        List<Vector3> path = new List<Vector3>();
-        Vector3 current = start;
-
-        while (current != end)
-        {
-            if (current.x != end.x)
-            {
-                current.x += Mathf.Sign(end.x - current.x);
-            }
-            if (current.y != end.y)
-            {
-                current.y += Mathf.Sign(end.y - current.y);
-            }
-            if (current.z != end.z)
-            {
-                current.z += Mathf.Sign(end.z - current.z);
-            }
-            path.Add(new Vector3(current.x, start.y, current.z));
-        }
-
-        foreach (var item in path) Debug.Log(item);
-
-        return path;
     }
 
     [Button]
