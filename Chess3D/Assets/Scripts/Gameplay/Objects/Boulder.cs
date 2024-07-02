840344113ab5f19ea81a631631c6b5f798271a9d
QuantumMarshal <quangnhan1411@gmail.com>
2024-07-02 16:28:38 +0700

    Update Box

--- Chess3D/Assets/Scripts/Gameplay/Objects/Boulder.cs
+++ Chess3D/Assets/Scripts/Gameplay/Objects/Boulder.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Boulder : GameplayObject
 {
     
-    public override void MoveAnim(Vector3 posIndexToMove, float speed, bool isRoundInteger = false) 
+    public override void MoveAnim(Vector3 posIndexToMove, float speed) 
         //posIndexToMove: ở đây không phải vị trí cuối cùng, mà chỉ là vị trí mà chessman đẩy đến (sát bên chessman),
         //sẽ phải tự tính toàn tiếp vị trí tiếp theo nếu dưới chân nó là NONE (thì sẽ rơi) hoặc SLOPE (thì sẽ lăn) với tốc độ defaultSpeed. 
         //speed: là speed của chessMan đẩy boulder, boulder sẽ di chuyển cùng tốc độ với chessman đẩy box,
--- Chess3D/Assets/Scripts/Gameplay/Objects/GameplayObject.cs
+++ Chess3D/Assets/Scripts/Gameplay/Objects/GameplayObject.cs
@@ -7,11 +7,11 @@ public class GameplayObject : MonoBehaviour
     public float defaultSpeed;
     public Outline outline;
 
-    public virtual void MoveAnim(Vector3 posIndexToMove, float speed, bool isRoundInteger = false)
+    public virtual void MoveAnim(Vector3 posIndexToMove, float speed)
     {
         Debug.Log("A");
     }
-    public virtual void MoveAnim(Vector3 posIndexToMove, float speed, bool isRoundInteger = false, ChessMan chessManAbove = null)
+    public virtual void MoveAnim(Vector3 posIndexToMove, float speed, ChessMan chessManAbove = null)
     {
     }
 
