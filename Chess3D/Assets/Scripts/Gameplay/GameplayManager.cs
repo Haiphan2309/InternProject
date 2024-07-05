8e3c23cc61ae41e47a7c59aa34b4fd45022b8265
Haiphan2309 <haiphan.230903@gmail.com>
2024-07-05 11:20:39 +0700

    + Add level 1 1

--- Chess3D/Assets/Scripts/Gameplay/GameplayManager.cs
+++ Chess3D/Assets/Scripts/Gameplay/GameplayManager.cs
@@ -497,10 +497,10 @@ public class GameplayManager : MonoBehaviour
 
     public int GetStarOfCurrentLevel()
     {
-        if (remainTurn == 0) return 0;
-        if (remainTurn < levelData.starTurn2) return 1;
-        if (remainTurn < levelData.starTurn3) return 2;
-        return 3;
+        if (remainTurn >= levelData.starTurn3) return 3;
+        if (remainTurn >= levelData.starTurn2) return 2;
+        if (remainTurn > 0) return 1;
+        return 0;
     }
     void Win()
     {
