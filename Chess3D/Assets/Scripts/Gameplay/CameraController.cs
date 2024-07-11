ed7a3077ea25fc0d36a918baca74238a3e892e7c
Phạm Phú Khang <ppkhang285@gmail.com>
2024-07-11 10:58:49 +0700

    update test camera moving

--- Chess3D/Assets/Scripts/Gameplay/CameraController.cs
+++ Chess3D/Assets/Scripts/Gameplay/CameraController.cs
@@ -40,6 +40,14 @@ public class CameraController : MonoBehaviour
     private float zoomVelocity = 0f;
     private float smoothZoomTime = 0.25f;
     //
+    private float moveVelocityX = 0f;
+    private float moveVelocityY = 0f;
+    private float moveMax = 1f;
+    private float moveMin = 0f;
+    private float smoothMoveTime = 0.25f;
+    [SerializeField] private float moveSpeedX = 10f;
+    [SerializeField] private float moveSpeedY = 10f;
+
     private bool isLocked = false;
    
     public void Setup(Vector3 center, float distance)
@@ -81,6 +89,7 @@ public class CameraController : MonoBehaviour
     {
         if (_camera != null && !isLocked)
         {
+            HandleMoveCamera();
             HandleZoomCamera();
             HandleSwipeCamera();
         }
@@ -88,7 +97,7 @@ public class CameraController : MonoBehaviour
     }
 
     // Hàm để giới hạn góc quay
-    protected static float ClampAngle(float angle, float min, float max)
+    private static float ClampAngle(float angle, float min, float max)
     {
         if (angle < -360F)
             angle += 360F;
@@ -97,7 +106,7 @@ public class CameraController : MonoBehaviour
         return Mathf.Clamp(angle, min, max);
     }
 
-    protected void HandleSwipeCamera()
+    private void HandleSwipeCamera()
     {
         if (Input.GetMouseButton(0) && !EventSystem.current.IsPointerOverGameObject()) // Kiểm tra xem nút chuột trái có được nhấn không
         {
@@ -116,28 +125,42 @@ public class CameraController : MonoBehaviour
         transform.rotation = rotation;
         transform.position = position;
     }
-    protected void HandleZoomCamera()
+    private void HandleZoomCamera()
     {
         if (Input.touchCount == 2 && !EventSystem.current.IsPointerOverGameObject())
         {
-            Debug.Log("Zoom");
+         
             Touch touchFirst = Input.GetTouch(0);
             Touch touchSecond = Input.GetTouch(1);
 
             Vector2 touchFirstPrePos = touchFirst.position - touchFirst.deltaPosition;
             Vector2 touchSecondPrePos = touchSecond.position - touchSecond.deltaPosition;
 
-            float preMagnitude = (touchFirstPrePos - touchSecondPrePos).magnitude;
-            float currentMagnitude = (touchFirst.position - touchSecond.position).magnitude;
+            Vector2 touchFirstVector = touchFirst.position - touchFirstPrePos;
+            Vector2 touchSecondVector = touchSecond.position - touchSecondPrePos;
+            if (Vector2.Angle(touchFirstVector, touchSecondVector) < 45f)
+            {
+                Debug.Log("MoveCamera");
+            }
+            else
+            {
+                float preMagnitude = (touchFirstPrePos - touchSecondPrePos).magnitude;
+                float currentMagnitude = (touchFirst.position - touchSecond.position).magnitude;
+                float diff = currentMagnitude - preMagnitude;
 
-            float diff = currentMagnitude - preMagnitude;
+                Zoom(diff * zoomSpeed);
+            }
 
-            Zoom(diff * zoomSpeed);
+            //
+
+
+            
         }
-
+        #if UNITY_EDITOR
         Zoom(Input.GetAxis("Mouse ScrollWheel")*zoomSpeed);
+        #endif
     }
-    protected void Zoom(float inc)
+    private void Zoom(float inc)
     {
     
         zoom -= inc;
@@ -145,6 +168,33 @@ public class CameraController : MonoBehaviour
         _camera.m_Lens.OrthographicSize = Mathf.SmoothDamp(_camera.m_Lens.OrthographicSize, zoom,  ref zoomVelocity, smoothZoomTime);
     }
 
+    private void HandleMoveCamera()
+    {
+#if UNITY_EDITOR
+
+        float x = Input.GetAxis("Horizontal") * moveSpeedX * 0.02f;
+        float y = Input.GetAxis("Vertical") * moveSpeedY * 0.02f;
+        Move(x, y);
+        
+#endif
+    }
+
+    private void Move(float x, float y)
+    {
+        CinemachineFramingTransposer framing = _camera.GetCinemachineComponent<CinemachineFramingTransposer>();
+
+        float targetX = framing.m_ScreenX - x;
+        float targetY = framing.m_ScreenY + y;
+
+        targetX = Mathf.Clamp(targetX, moveMin, moveMax);
+        targetY = Mathf.Clamp(targetY, moveMin, moveMax);
+
+        float smoothX = Mathf.SmoothDamp(framing.m_ScreenX, targetX, ref moveVelocityX, smoothMoveTime);
+        float smoothY = Mathf.SmoothDamp(framing.m_ScreenY, targetY, ref moveVelocityY, smoothMoveTime);
+
+        framing.m_ScreenX = smoothX;
+        framing.m_ScreenY = smoothY;
+    }
     public void Lock()
     {
         isLocked = true;
