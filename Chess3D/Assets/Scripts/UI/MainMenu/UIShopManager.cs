659aa327e43431848b071fd5e9a89b711730b1c1
Haiphan2309 <haiphan.230903@gmail.com>
2024-07-17 11:37:28 +0700

    + Update shop

--- Chess3D/Assets/Scripts/UI/MainMenu/UIShopManager.cs
+++ Chess3D/Assets/Scripts/UI/MainMenu/UIShopManager.cs
@@ -1,4 +1,5 @@
 using DG.Tweening;
+using GDC.Managers;
 using NaughtyAttributes;
 using System.Collections;
 using System.Collections.Generic;
@@ -11,13 +12,26 @@ public class UIShopManager : MonoBehaviour
     [SerializeField] RectTransform rect, contentRect;
     [SerializeField] UIShopSlot shopSlotprefab;
 
-    [SerializeField] Button exitButton;
+    [SerializeField] Button exitButton, removeAds;
+    [SerializeField] LanguageDictionary removeAdsDict;
 
     [Button]
     public void Show()
     {
         exitButton.onClick.RemoveAllListeners();
         exitButton.onClick.AddListener(Hide);
+
+        removeAds.onClick.RemoveAllListeners();
+        removeAds.onClick.AddListener(RemoveAds);
+
+        if (SaveLoadManager.Instance.GameData.isPurchaseAds)
+        {
+            removeAds.interactable = false;
+        }
+        else
+        {
+            removeAds.interactable = true;
+        }
 
         rect.localScale = Vector2.zero;
         rect.DOScale(1, 0.5f).SetEase(Ease.OutBack);
@@ -36,5 +50,13 @@ public class UIShopManager : MonoBehaviour
     public void Hide()
     {
         rect.DOScale(0, 0.5f).SetEase(Ease.InBack);
+    }
+
+    void RemoveAds()
+    {
+        PopupManager.Instance.ShowAnnounce(removeAdsDict[SaveLoadManager.Instance.GameData.language]);
+        SaveLoadManager.Instance.GameData.isPurchaseAds = true;
+        SaveLoadManager.Instance.Save();
+        removeAds.interactable = false;
     }
 }
--- Chess3D/Assets/Scripts/UI/MainMenu/UIShopSlot.cs
+++ Chess3D/Assets/Scripts/UI/MainMenu/UIShopSlot.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using GDC.Managers;
 using NaughtyAttributes;
 using System.Collections;
@@ -8,16 +9,27 @@ using UnityEngine.UI;
 
 public class UIShopSlot : MonoBehaviour
 {
+    [SerializeField] RectTransform rect;
     [SerializeField] Image iconImage, bgImage;
     [SerializeField] TMP_Text amountText, costText;
     [SerializeField, ReadOnly] ShopSlotData shopSlotData;
+
+    [SerializeField] LanguageDictionary thankYouDict, sorryDict;
 
     public void Setup(ShopSlotData shopSlotData)
     {
         this.shopSlotData = shopSlotData;
         iconImage.sprite = shopSlotData.icon;
         bgImage.sprite = shopSlotData.bg;
-        amountText.text = shopSlotData.slotName + " x" + shopSlotData.amount.ToString();
+        if (shopSlotData.undoAmount > 0)
+        {
+            amountText.text = shopSlotData.slotName + " x" + shopSlotData.undoAmount.ToString();
+        }
+        else
+        {
+            amountText.text = shopSlotData.slotName + " x" + shopSlotData.solveAmount.ToString();
+        }   
+        
         switch (SaveLoadManager.Instance.GameData.language)
         {
             case GDC.Enums.Language.English:
@@ -29,4 +41,21 @@ public class UIShopSlot : MonoBehaviour
         }
 
     }
+    public void OnDown()
+    {
+        rect.DOScale(0.9f, 0.2f);
+    }
+    public void OnExit()
+    {
+        rect.DOScale(1, 0.25f).SetEase(Ease.OutBack);
+    }
+    public void OnClick()
+    {
+        rect.DOScale(1, 0.25f).SetEase(Ease.OutBack);
+        Debug.Log("Da thanh toan so tien " + costText.text + " va mua duoc " + amountText.text);
+        PopupManager.Instance.ShowAnnounce(thankYouDict[SaveLoadManager.Instance.GameData.language]);
+        SaveLoadManager.Instance.GameData.undoNum += shopSlotData.undoAmount;
+        SaveLoadManager.Instance.GameData.solveNum += shopSlotData.solveAmount;      
+        SaveLoadManager.Instance.Save();
+    }    
 }
