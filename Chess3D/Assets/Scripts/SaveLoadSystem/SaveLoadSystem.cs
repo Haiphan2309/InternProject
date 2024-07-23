c8d4204daff7d413b206d99af9dae574348de47f
Haiphan2309 <haiphan.230903@gmail.com>
2024-07-23 16:07:51 +0700

    * Fix save load for android

--- Chess3D/Assets/Scripts/SaveLoadSystem/SaveLoadSystem.cs
+++ Chess3D/Assets/Scripts/SaveLoadSystem/SaveLoadSystem.cs
@@ -19,6 +19,15 @@ namespace GDC.Managers
         // Update is called once per frame
         static public void SaveData(GameDataOrigin gameDataOrigin)
         {
+#if UNITY_ANDROID
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            string path = Application.persistentDataPath + "/dataPoint.fun";
+            FileStream stream = new FileStream(path, FileMode.Create);
+
+            formatter.Serialize(stream, gameDataOrigin);
+            stream.Close();
+#else
             string folderName = "SaveData";
             string fileName = "Data";
             if (!Directory.Exists(folderName))
@@ -32,24 +41,41 @@ namespace GDC.Managers
             dataFile.Close();
             //print("Save data to: " + Directory.GetCurrentDirectory().ToString() + "/" + folderName + "/" + fileName + ".bin");
             //print("Value1 and Value2: " + gameData.sliderValue1 + " " + gameData.sliderValue2);
+#endif
         }
 
-        static public void LoadData(GameDataOrigin gameDataOrigin)
+        static public void LoadData(out GameDataOrigin gameDataOrigin)
         {
+#if UNITY_ANDROID
+            string path = Application.persistentDataPath + "/dataPoint.fun";
+            gameDataOrigin = new GameDataOrigin();
+            if (File.Exists(path))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+
+                FileStream stream = new FileStream(path, FileMode.Open);
+
+                gameDataOrigin = (GameDataOrigin)formatter.Deserialize(stream);
+                stream.Close();
+
+                SaveLoadManager.Instance.GameDataOrigin = gameDataOrigin;
+            }
+#else
             string folderName = "SaveData";
             string fileName = "Data";
             if (!Directory.Exists(folderName))
             {
                 Logger.Log("Don't have data to load");
+                gameDataOrigin = new GameDataOrigin();
                 return;
             }
 
             FileStream dataFile = File.Open(folderName + "/" + fileName + ".bin", FileMode.Open);
             gameDataOrigin = (GameDataOrigin)formatter.Deserialize(dataFile);
        
-            SaveLoadManager.Instance.GameDataOrigin = gameDataOrigin;
+            //SaveLoadManager.Instance.GameDataOrigin = gameDataOrigin;
             dataFile.Close();
-
+#endif
         }
     }
 }
